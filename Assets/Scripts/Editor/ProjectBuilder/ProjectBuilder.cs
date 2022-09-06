7f4788ce5ff2169905d25985fb641d1796c6a035
alexanian <alexanyanyuka@gmail.com>
2022-09-06 12:49:18 +0200

    Building android

--- Assets/Scripts/Editor/ProjectBuilder/ProjectBuilder.cs
+++ Assets/Scripts/Editor/ProjectBuilder/ProjectBuilder.cs
@@ -1,16 +1,45 @@
 namespace Editor.ProjectBuilder
 {
+    using System.Collections.Generic;
     using UnityEditor;
     using UnityEngine;
 
+
     public class ProjectBuilder : MonoBehaviour
     {
-        [MenuItem("MyMenu/Do Something")]
-        public static void Build()
+        [MenuItem("Build/Android Build")]
+        public static void BuildAndroid()
         {
             string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-            string[] levels = new string[] {"Assets/Scenes/AppScene.unity"};
-            BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.apk", BuildTarget.Android, BuildOptions.None);
+            string[] levels = GetAllScenes();
+            EditorUserBuildSettings.development = false;
+            EditorUserBuildSettings.buildAppBundle = true;
+            BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.aab", BuildTarget.Android, BuildOptions.None);
+        }
+        
+        [MenuItem("Build/iOS Build")]
+        public static void BuildIOS()
+        {
+            
+        }
+
+        private static string[] GetAllScenes()
+        {
+            List<string> scenes = new List<string>();
+            foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            {
+                if (scene.enabled)
+                {
+                    scenes.Add(scene.path);
+                }
+            }
+
+            return scenes.ToArray();
+        }
+
+        private void GetParamsFile()
+        {
+            
         }
     }
     
